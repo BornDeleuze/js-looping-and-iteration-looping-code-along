1c7ced9fd3f562efa7682261da8d17a02378de1c
Matt Beaudoin <ehxtwo@gmail.com>
2021-06-04 11:42:22 -0700

    commit tree a9fa631e7d3f39a2ee38a1384b0c98a21778ba6c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+let messages = []
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages
+}
+
+function countDown(number){
+  while (number > -1){
+    console.log(number)
+    number = number-1
+  }
+
+}
